41ce558393d9d22aba6e1b469041d0c42580485d
Sukhvindra9643 <20b1029.bca1@gitmgurgaon.com>
2023-04-10 02:56:38 +0530

    user part done

---
+++ utils/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  console.log(process.env.SMTP_PASSWORD);
+
+  var transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  var mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  let info =  await transporter.sendMail(mailOptions);
+  console.log("Message sent: %s", info.messageId);
+  return info.messageId;
+};
+module.exports = sendEmail;
